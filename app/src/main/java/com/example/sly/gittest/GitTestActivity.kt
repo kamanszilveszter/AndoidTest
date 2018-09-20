3599b5135b0e5ca5a8398204819efb0ebca96fd6
Sly <kaman.szilveszter@gmail.com>
2018-09-20 02:56:11 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/sly/gittest/GitTestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sly.gittest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GitTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_test)
+    }
+}
